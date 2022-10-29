40d884ff33f9539dc5fcdb50156d7d04ec7aba8e
Jack Mahedy <jackmahedy570@gmail.com>
2022-10-29 14:57:19 -0400

    Finished Lab

--- Lab0/src/Lab0.java
+++ Lab0/src/Lab0.java
@@ -2,6 +2,6 @@ public class Lab0{
     public static void main(String[] args){
         System.out.println("Hi. My name is Jack Mahedy.");
         System.out.println("My major is Computer Information Technology.");
-        System.out.println("My favorite programming language is Java.");
+        System.out.println("My favorite programming language is Java!");
     }
 }
